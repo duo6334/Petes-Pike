511092e67bea4ebb509f56d348c84848f3130543
Abbyleila <abrielleriv@gmail.com>
2025-11-03 19:36:49 -0500

    move

---
+++ src/main/java/petespike/model/Move.java
@@ -0,0 +1,19 @@
+package petespike.model;
+
+public class Move {
+    private Position position;
+    private Direction direction;
+
+    public Move(Position position, Direction direction){
+        this.position = position;
+        this.direction = direction;
+    }
+
+    public Position getPosition(){
+        return this.position;
+    }
+
+    public Direction getDirection(){
+        return this.direction;
+    }
+}

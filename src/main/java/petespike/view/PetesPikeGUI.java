98a928871b5f51acf2af0841fead785ae2f7bb07
Abbyleila <abrielleriv@gmail.com>
2025-11-11 11:23:26 -0500

    GUI file made

---
+++ src/main/java/petespike/view/PetesPikeGUI.java
@@ -0,0 +1,17 @@
+package petespike.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PetesPikeGUI extends Application{
+    // size of the board max of 9
+    private static final int MAX_SIZE = 9;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
+
+}

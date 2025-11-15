4bb722d4ad16d5cce85c4d1db943789205948e02
duo6334 <duo6334@rit.edu>
2025-11-15 15:49:31 -0500

    Observer notifier done

--- src/main/java/petespike/model/PetesPikeObserver.java
+++ src/main/java/petespike/model/PetesPikeObserver.java
@@ -2,4 +2,12 @@ package petespike.model;
 
 public interface PetesPikeObserver {
 
+    /**
+     * Method is called by PetesPike class when a piece 
+     * has been successfully moved
+     * 
+     * @param from  The position a piece is moving from
+     * @param to The position a piece is moving to
+     */
+    void pieceMoved(Position from, Position to);
 }
